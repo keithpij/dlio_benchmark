a4377dd117b0d14c9f1a3162ffbb5727b974218c
Keith Pijanowski <keithpij@gmail.com>
2025-10-16 10:01:16 -0400

    Logger fix to s3_stage.py.

--- dlio_benchmark/plugins/experimental/src/data_loader/s3/s3_stage.py
+++ dlio_benchmark/plugins/experimental/src/data_loader/s3/s3_stage.py
@@ -70,6 +70,9 @@ def main():
     '''
     Main function that processes the arguments sent to this module via the command line.
     '''
+    # Create the logger.
+    du.create_logger(use_file=True)
+
     parser = argparse.ArgumentParser(description='ML Command line interface.')
     parser.add_argument('-lb', '--list_buckets', help='List all buckets.', action='store_true')
     parser.add_argument('-lo', '--list_objects', help='List all objects in the specified bucket.', action='store_true')
